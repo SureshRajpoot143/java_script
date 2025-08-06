3449ba3dcc556265dfe826af3a7e2dfd0e2d745c
suresh rajpoot <imssr1005@gmail.com>
2025-08-06 12:00:46 +0530

    Create age.js

---
+++ age.js
@@ -0,0 +1,12 @@
+ check an age is greater than 18  or not
+
+let age=30;
+if(age<18){
+    console.log("print")
+}
+else if(age<35){
+    console.log("not print")
+}
+else{
+    console.log("print")
+} 
